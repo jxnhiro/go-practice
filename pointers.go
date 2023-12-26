a3c9e4fe8aef6fb49c988e18d95ab254a072bd2a
Kenny Jinhiro <jxnhiro@gmail.com>
2023-12-26 10:50:18 +0700

    Saving memory with pointers

--- pointers.go
+++ pointers.go
@@ -5,15 +5,15 @@ import "fmt"
 func main() {
 	age := 32
 
-	var agePointer *int
-	agePointer = &age
+	var agePointer *int = &age
 
-	fmt.Println("Age:", agePointer)
-	adultYears := getAdultYears(age)
+	fmt.Println("Age:", *agePointer)
+	adultYears := getAdultYears(agePointer)
 	fmt.Println("Adult Years:", adultYears)
 }
 
-func getAdultYears(age int) (adultYears int) {
-	adultYears = age - 18
+//No copy of 32 made
+func getAdultYears(age *int) (adultYears int) {
+	adultYears = *age - 18
 	return adultYears
 }
